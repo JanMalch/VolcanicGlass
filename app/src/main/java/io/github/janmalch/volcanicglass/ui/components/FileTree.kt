3d07a4333eb493f0e32777d5da84ce288ecbc42a
JanMalch <25508038+JanMalch@users.noreply.github.com>
2026-04-25 11:56:27 +0200

    feat: make folders collapsible in tree

--- app/src/main/java/io/github/janmalch/volcanicglass/ui/components/FileTree.kt
+++ app/src/main/java/io/github/janmalch/volcanicglass/ui/components/FileTree.kt
@@ -1,19 +1,26 @@
 package io.github.janmalch.volcanicglass.ui.components
 
 import android.net.Uri
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -38,23 +45,35 @@ private fun ColumnScope.FileTreeNode(
     onFileClick: (TreeState.Success.Node) -> Unit,
     paddingStart: Dp,
 ) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val iconRotation by animateFloatAsState(
+        if (isExpanded) 0f else -90f,
+        label = "icon_rotation__${node.name}"
+    )
     Row(
         horizontalArrangement = Arrangement.spacedBy(4.dp),
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
+            .fillMaxWidth()
             .run {
-                if (node.isDirectory) this
+                if (node.isDirectory) clickable { isExpanded = !isExpanded }
                 else clickable { onFileClick(node) }
             }
             .padding(start = paddingStart)
     ) {
         if (node.isDirectory) {
-            Icon(Icons.Default.KeyboardArrowDown, contentDescription = null)
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowDown,
+                contentDescription = null,
+                modifier = Modifier.rotate(iconRotation)
+            )
         }
         Text(text = node.name)
     }
+    if (isExpanded) {
     for (child in node.children) {
         FileTreeNode(child, onFileClick, paddingStart = paddingStart + 28.dp)
+    }
     }
 }
 
